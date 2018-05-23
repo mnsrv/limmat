ffe50abc0ba603d52a4c6f5ccd59ba81ce5a2e14
Aleksandr Mansurov <sasha.mansurov@gmail.com>
2018-05-23 07:21:48 +0300

    Add faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  Transaction.create({
+    date: Faker::Date.backward(14),
+    amount: Faker::Number.between(10, 400),
+    memo: Faker::Company.name
+  })
+end
