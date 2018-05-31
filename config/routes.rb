06a62a39367cd371e08424dfa2e8bdeb35d5853e
Aleksandr Mansurov <sasha.mansurov@gmail.com>
2018-06-01 01:16:33 +0300

    Add create user route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :v1, defaults: { format: :json } do
     resources :transactions
     resource :sessions, only: [:show, :create, :destroy]
+    resources :users, only: [:create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
